05df2983b0515bc959573abd4dd76bbb7932f5b3
Satoki <satokiito@gmail.com>
2023-11-23 14:43:01 +0000

    added content to 'about me' section with some styling

--- individual-portfolio/src/components/main/index.jsx
+++ individual-portfolio/src/components/main/index.jsx
@@ -1,21 +1,41 @@
 import Overview from './components/Overview'
+import HTMLLogo from '../../assets/tech-stack-logos/html-logo.svg'
+import CSSLogo from '../../assets/tech-stack-logos/css-logo.svg'
+import JSLogo from '../../assets/tech-stack-logos/js-logo.svg'
+import ReactLogo from '../../assets/tech-stack-logos/react-logo.svg'
+import GitLogo from '../../assets/tech-stack-logos/git-logo.svg'
 import '../../styles/main/main.css'
+import { useState } from 'react'
 
 function Main() {
+
+    const [showTechStackText, setShowTechStackText] = useState(null)
+
+    function removeText() {
+        setShowTechStackText(null)
+    }
+
+    function toggleText(e) {
+        const { name } = e.target
+        setShowTechStackText(name)
+    }
 
     return (
         <main className='main grid'>
             <Overview></Overview>
-            <section id='about-me' className='about-me'>
-                <h2>About Me</h2>
+            <section id='about-me' className='about-me grid'>
+                <h2 className='about-me-header'>About Me</h2>
                 <p>I have learnt a range of skills in my journey so far. I have experience working with:</p>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Git</li>
-                </ul>
+                <div onMouseOut={removeText} className='tech-stack-logos grid'>
+                    <img className='js-logo' name='JavaScript' onMouseOver={toggleText}  src={JSLogo} alt="js logo" width={200} />
+                    <img className='html-logo' name='HTML' onMouseOver={toggleText}  src={HTMLLogo} alt="html logo" width={200}/>
+                    <img className='git-logo' name='Git' onMouseOver={toggleText}  src={GitLogo} alt="git logo" width={200} />
+                    <img className='css-logo' name='CSS' onMouseOver={toggleText}  src={CSSLogo} alt="css logo" width={200}/>
+                    <img className='react-logo' name='React' onMouseOver={toggleText}  src={ReactLogo} alt="react logo" width={200} />
+                </div>
+                <div className='tech-stack-text grid'>
+                    {showTechStackText}
+                </div>
                 <p>Have a look at some of my work below:</p>
             </section>
             <section id='projects' className='projects'>
@@ -24,6 +44,16 @@ function Main() {
                 <div>blah blah blah blah blah </div>
                 <div>blah blah blah blah blah </div>
                 <h3>My current work-in-progress:</h3>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
+                <div>blah blah blah blah blah </div>
                 <div>blah blah blah blah blah </div>
             </section>
         </main>
